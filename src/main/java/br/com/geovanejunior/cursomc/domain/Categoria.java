1b6434201817727f3d9d80295e291bf4a9f36371
Geovane Junior <geovane.gjunior@gmail.com>
2020-11-29 11:30:33 -0300

    Incluindo Banco de dados H2 e criação automática da base de dados

--- src/main/java/br/com/geovanejunior/cursomc/domain/Categoria.java
+++ src/main/java/br/com/geovanejunior/cursomc/domain/Categoria.java
@@ -2,15 +2,21 @@ package br.com.geovanejunior.cursomc.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
-
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 
