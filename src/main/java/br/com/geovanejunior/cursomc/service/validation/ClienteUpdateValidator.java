40b1b45c082021f7a58ae04ff8edf6cee68cb96b
Geovane Junior <geovane.gjunior@gmail.com>
2020-12-13 18:55:21 -0300

    Validação customizada: email não repetido na atualização do Cliente

--- src/main/java/br/com/geovanejunior/cursomc/service/validation/ClienteUpdateValidator.java
+++ src/main/java/br/com/geovanejunior/cursomc/service/validation/ClienteUpdateValidator.java
@@ -6,16 +6,22 @@ import br.com.geovanejunior.cursomc.dto.ClienteDTO;
 import br.com.geovanejunior.cursomc.repositories.ClienteRepository;
 import br.com.geovanejunior.cursomc.resources.exceptions.FieldMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
     @Override
     public void initialize(ClienteUpdate ann) {
@@ -23,13 +29,19 @@ public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate
 
     @Override
     public boolean isValid(ClienteDTO clienteDTO, ConstraintValidatorContext context) {
+
+        @SuppressWarnings("unchecked")
+        Map<String, String> map = (Map<String, String>) httpServletRequest.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+
+        Long uriId = Long.parseLong(map.get("id"));
+
         List<FieldMessage> list = new ArrayList<>();
 
         // Validação de existência do email informando em base de dados
 
         Cliente aux = clienteRepository.findByEmail(clienteDTO.getEmail());
 
-        if (aux != null) {
+        if (aux != null && !aux.getId().equals(uriId)) {
             list.add(new FieldMessage("email", "Email informado (" + clienteDTO.getEmail() + ") já existente"));
         }
 
