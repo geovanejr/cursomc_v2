a2fe0c8e01f12109cb85b28f505f0bfe788849e7
Geovane Junior <geovane.gjunior@gmail.com>
2020-12-19 14:15:28 -0300

    Criando profile de teste

---
+++ src/main/java/br/com/geovanejunior/cursomc/config/TestConfig.java
@@ -0,0 +1,24 @@
+package br.com.geovanejunior.cursomc.config;
+
+import br.com.geovanejunior.cursomc.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public Boolean instantiateDatabase() throws ParseException {
+
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
