4bd6171347ef5d628788473eaaa8c46706906caa
Geovane Junior <geovane.gjunior@gmail.com>
2020-12-21 07:46:19 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/geovanejunior/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package br.com.geovanejunior.cursomc.config;
+
+import br.com.geovanejunior.cursomc.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public Boolean instantiateDatabase() throws ParseException {
+
+        if (!strategy.equals("create")) {
+
+            return false;
+        }
+
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
